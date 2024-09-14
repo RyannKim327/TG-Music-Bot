26105b9aba2dc0235df50f6d7d6e712a581acd87
RyannKim327 <rksesgundo123@gmail.com>
2024-09-14 08:47:52 +0800

    09-14-24 08:47 [Initial Commit]

---
+++ index.js
@@ -0,0 +1,56 @@
+const tg = require("node-telegram-bot-api");
+const fs = require("fs");
+
+const music = require("./src/music");
+
+const token = "7363842676:AAHMYrc-QqcBoIYa_1TTX8_YMuFpkqVW-2g";
+
+const start = async () => {
+  console.log("+-----------------------------------------+");
+  console.log("|      Welcome to Telegram Music Bot      |");
+  console.log("|     Developed by Ryann Kim Sesgundo     |");
+  console.log("+-----------------------------------------+");
+
+  const directory = `${__dirname}/temp`;
+
+  try {
+    const api = new tg(token, {
+      polling: true,
+    });
+
+    if (fs.existsSync(directory)) {
+      fs.rm(directory, { recursive: true }, (e) => {});
+    }
+
+    setTimeout(() => {
+      fs.mkdirSync(directory);
+    }, 1000);
+
+    api.onText(/([\w\W]+)/gi, (msg, match) => {
+      if (match[0].startsWith("/start")) {
+        api
+          .sendMessage(
+            msg.chat.id,
+            "Greetings, I am VibeSync, your telegram bot music (based on youtube music). If you want to get started, kindly chat your music you want to look for.",
+          )
+          .then((r) => {
+            setTimeout(() => {
+              api.deleteMessage(r.chat.id, r.message_id);
+              console.log(`LOG: ${JSON.stringify(r)}`);
+            }, 10000);
+          })
+          .catch((e) => {});
+      } else {
+        if (match[1]) {
+          music(api, msg, match[0]);
+        } else {
+          api.sendMessage(msg.chat.id, "Invalid message, please try again");
+        }
+      }
+    });
+  } catch (error) {
+    console.error(`ERR: ${error}`);
+  }
+};
+
+start();
---
+++ src/music.js
@@ -0,0 +1,119 @@
+const fs = require("fs");
+const { Innertube, UniversalCache, Utils } = require("youtubei.js");
+
+const logs = (_logs) => {
+  let current = fs.readFileSync("logs.txt", "utf8");
+  const t = new Date();
+  const time = `${t.getHours()}:${t.getMinutes()}`;
+  current += `[${time}] ${_logs}\n`;
+  fs.writeFileSync("logs.txt", current, "utf8");
+};
+
+module.exports = async (api, msg, search) => {
+  try {
+    const yt = await Innertube.create({
+      cache: new UniversalCache(false),
+      generate_session_locally: true,
+    });
+
+    const s = await yt.music.search(search, {
+      type: "video",
+    });
+
+    const info = await s.contents;
+    let i = info[0];
+    let j = 0;
+
+    while (i.type != "MusicShelf") {
+      j++;
+      i = info[j];
+    }
+
+    const details = i.contents[0];
+
+    console.log(`Found [INFO]: ${details.id}`);
+
+    const stream = await yt.download(`${details.id}`, {
+      type: "audio",
+      quality: "bestefficiency",
+      format: "mp4",
+      client: "YTMUSIC",
+    });
+
+    const name = `${__dirname}/../temp/${details.title.replace(/([\s\W]+)/gi, "_")}_${details.authors[0].name.replace(/([\s\W]+)/gi, "_")}.mp3`;
+    const file = fs.createWriteStream(name);
+
+    for await (const chunk of Utils.streamToIterable(stream)) {
+      file.write(chunk);
+    }
+
+    api
+      .deleteMessage(msg.chat.id, msg.message_id)
+      .then((r) => {
+        console.log(`INFO: ${r}`);
+        logs(`INFO: ${r}`);
+      })
+      .catch((e) => {
+        logs(`ERR: ${e}`);
+        api.sendMessage(msg.chat.id, JSON.stringify(e));
+      });
+
+    api
+      .sendMessage(msg.chat.id, `Found [INFO]: ${details.title}`)
+      .then((r) => {
+        setTimeout(() => {
+          api.deleteMessage(r.chat.id, r.message_id);
+        }, 5000);
+      })
+      .catch((e) => {});
+
+    api
+      .sendAudio(msg.chat.id, fs.createReadStream(name), {}, {})
+      .then((r) => {
+        console.log(`INFO: ${JSON.stringify(r)}`);
+        logs(`INFO: ${JSON.stringify(r)}`);
+        if (fs.existsSync(name)) {
+          setTimeout(() => {
+            if (fs.existsSync(name)) {
+              fs.unlinkSync(name, (e) => {});
+            }
+          }, 2500);
+        }
+      })
+      .catch((error) => {
+        if (fs.existsSync(name)) {
+          setTimeout(() => {
+            fs.unlinkSync(name, (e) => {});
+          }, 2500);
+        }
+        api
+          .sendMessage(msg.chat.id, `ERR: ${error}`)
+          .then((r) => {
+            setTimeout(() => {
+              api.deleteMessage(r.chat.id, r.message_id);
+            }, 5000);
+          })
+          .catch((e) => {});
+        logs(`ERR: ${error}`);
+      });
+
+    setTimeout(() => {
+      if (fs.existsSync(name)) {
+        fs.unlinkSync(name, (e) => {});
+      }
+    }, 10000);
+  } catch (error) {
+    logs(`ERR: ${error}`);
+    api
+      .sendMessage(msg.chat.id, `ERR: ${error}`)
+      .then((r) => {
+        setTimeout(() => {
+          api.deleteMessage(r.chat.id, r.message_id);
+        }, 5000);
+      })
+      .catch((e) => {
+        console.error(`ERR: ${JSON.stringify(e)}`);
+      });
+    // await api.deleteMessage(msg.chat.id, msg.message_id);
+  }
+};
