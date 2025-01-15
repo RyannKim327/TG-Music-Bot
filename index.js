bea0115a5ae1ce0e2e3561313efaf20d25374a97
RyannKim327 <rksesgundo123@gmail.com>
2025-01-16 01:05:12 +0800

    01-16-25 01:05

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const start = async () => {
       res.sendStatus(200)
     })
 
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log("HIHI")
     })
 
