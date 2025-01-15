6a07a376b50a3542d6020ff55691f856e45d1358
RyannKim327 <rksesgundo123@gmail.com>
2025-01-16 06:05:08 +0800

    01-16-25 06:05

--- src/music.js
+++ src/music.js
@@ -3,39 +3,44 @@ const fs = require("fs");
 const http = require("https");
 
 module.exports = async (api, msg, search) => {
+  api.sendMessage(msg.chat.is, "Please wait for a moment...").then(r => {
+    setTimeout(() => {
+      api.deleteMessage(r.chat.id, r.message_id)
+    }, 2500)
+  }).catch(e => {})
   try{
-  const { data } = await axios.get(
-    `https://dlvc.vercel.app/yt-audio?search=${encodeURI(search)}`,
-  );
-  api
-    .sendMessage(msg.chat.id, `Trial search [${search}]`)
-    .then((r) => {
-      setTimeout(() => {
-        api.deleteMessage(r.chat.id, r.message_id);
-      }, 2500);
-    })
-    .catch((e) => {});
+    const { data } = await axios.get(
+      `https://dlvc.vercel.app/yt-audio?search=${encodeURI(search)}`,
+    );
+    api
+      .sendMessage(msg.chat.id, `Trial search [${search}]`)
+      .then((r) => {
+        setTimeout(() => {
+          api.deleteMessage(r.chat.id, r.message_id);
+        }, 2500);
+      })
+      .catch((e) => {});
 
-  const filename = `${__dirname}/../temp/${data.title.replace(/\W/gi, "_")}.mp3`;
-  const file = fs.createWriteStream(filename);
-  http.get(data.downloadUrl, (res) => {
-    res.pipe(file);
-    file.on("finish", () => {
-      api
-        .sendAudio(msg.chat.id, fs.createReadStream(filename), {}, {})
-        .then((r) => {
-          console.log(`INFO: ${JSON.stringify(r)}`);
-          if (fs.existsSync(filename)) {
-            setTimeout(() => {
-              if (fs.existsSync(filename)) {
-                fs.unlinkSync(filename, (e) => {});
-              }
-            }, 10000);
-          }
-        })
-        .catch((error) => {});
+    const filename = `${__dirname}/../temp/${data.title.replace(/\W/gi, "_")}.mp3`;
+    const file = fs.createWriteStream(filename);
+    http.get(data.downloadUrl, (res) => {
+      res.pipe(file);
+      file.on("finish", () => {
+        api
+          .sendAudio(msg.chat.id, fs.createReadStream(filename), {}, {})
+          .then((r) => {
+            console.log(`INFO: ${JSON.stringify(r)}`);
+            if (fs.existsSync(filename)) {
+              setTimeout(() => {
+                if (fs.existsSync(filename)) {
+                  fs.unlinkSync(filename, (e) => {});
+                }
+              }, 10000);
+            }
+          })
+          .catch((error) => {});
+      });
     });
-  });
   }catch(e){
     console.error(`Error [Music]: ${e}`)
   }
