946a0f3143f1511a34b97d64e7758a9d30dc7233
RyannKim327 <rksesgundo123@gmail.com>
2025-01-25 18:48:30 +0800

    01-25-25 18:48

--- src/music.js
+++ src/music.js
@@ -3,15 +3,27 @@ const fs = require("fs");
 const http = require("https");
 
 module.exports = async (api, msg, search) => {
-  api.sendMessage(msg.chat.id, "Please wait for a moment...").then(r => {
-    setTimeout(() => {
-      api.deleteMessage(r.chat.id, r.message_id)
-    }, 2500)
-  }).catch(e => {})
-  try{
-    const { data } = await axios.get(
-      `https://dlvc.vercel.app/yt-audio?search=${encodeURI(search)}`,
-    );
+  api
+    .sendMessage(msg.chat.id, "Please wait for a moment...")
+    .then((r) => {
+      setTimeout(() => {
+        api.deleteMessage(r.chat.id, r.message_id);
+      }, 2500);
+    })
+    .catch((e) => {});
+  try {
+    let q = "?q=";
+    const data = await axios
+      .get(
+        `https://kaiz-apis.gleeze.com/api/ytsearch${q}${encodeURIComponent(result[1])}`,
+      )
+      .then((r) => {
+        return r.data.items[0];
+      })
+      .catch((err) => {
+        return null;
+      });
+
     api
       .sendMessage(msg.chat.id, `Trial search [${search}]`)
       .then((r) => {
@@ -21,9 +33,40 @@ module.exports = async (api, msg, search) => {
       })
       .catch((e) => {});
 
+    if (!data) {
+      return api
+        .sendMessage(msg.chat.id, `There's an error occured`)
+        .then((r) => {
+          setTimeout(() => {
+            api.deleteMessage(r.chat.id, r.message_id);
+          }, 2500);
+        })
+        .catch((e) => {});
+    }
+    const newData = await axios
+      .get(
+        `https://kaiz-ytmp4-downloader.vercel.app/ytmp4?url=${encodeURIComponent(data.url)}&quality=mp3`,
+      )
+      .then((res) => {
+        return res.data;
+      })
+      .catch((error) => {
+        return null;
+      });
+    if (!data) {
+      return api
+        .sendMessage(msg.chat.id, `There's an error occured`)
+        .then((r) => {
+          setTimeout(() => {
+            api.deleteMessage(r.chat.id, r.message_id);
+          }, 2500);
+        })
+        .catch((e) => {});
+    }
     const filename = `${__dirname}/../temp/${data.title.replace(/\W/gi, " ").trim().replace(/ /, "_")}.mp3`;
     const file = fs.createWriteStream(filename);
-    http.get(data.downloadUrl, (res) => {
+
+    http.get(newData.download_url, (res) => {
       res.pipe(file);
       file.on("finish", () => {
         api
@@ -41,12 +84,14 @@ module.exports = async (api, msg, search) => {
           .catch((error) => {});
       });
     });
-  }catch(e){
-    api.sendMessage(msg.chat.id, `Error [Music]: ${JSON.stringify(e, null, 2)}`).then(r => {
-      setTimeout(() => {
-        api.deleteMessage(r.chat.id, r.message_id)
-      }, 5000)
-    })
-    console.error(`Error [Music]: ${e}`)
+  } catch (e) {
+    api
+      .sendMessage(msg.chat.id, `Error [Music]: ${JSON.stringify(e, null, 2)}`)
+      .then((r) => {
+        setTimeout(() => {
+          api.deleteMessage(r.chat.id, r.message_id);
+        }, 5000);
+      });
+    console.error(`Error [Music]: ${e}`);
   }
 };
