82ea75960e4a29d671cb8ae676be67f8800863c8
RyannKim327 <rksesgundo123@gmail.com>
2025-01-31 00:09:24 +0800

    01-31-25  0:09

--- src/music.js
+++ src/music.js
@@ -4,112 +4,83 @@ const http = require("https");
 
 module.exports = async (api, msg, search) => {
   api
-    .sendMessage(msg.chat.id, "Please wait for a moment...")
-    .then(async (mainApi) => {
-      let q = "?q=";
-      const data = await axios
-        .get(
-          `https://kaiz-apis.gleeze.com/api/ytsearch${q}${encodeURIComponent(search)}`,
-        )
-        .then((r) => {
-          return r.data.items[0];
-        })
-        .catch((err) => {
-          return null;
-        });
-
-      api
-        .sendMessage(msg.chat.id, `Trial search [${search}]`)
-        .then((r) => {
-          setTimeout(() => {
-            api.deleteMessage(r.chat.id, r.message_id);
-          }, 2500);
-        })
-        .catch((e) => {});
-
-      if (!data) {
-        return api
-          .editMessageText(
-            `There's an error occured`,
-            mainApi.chat.id,
-            mainApi.message_id,
-          )
-          .then((r) => {
-            setTimeout(() => {
-              api.deleteMessage(r.chat.id, r.message_id);
-            }, 2500);
-          })
-          .catch((e) => {});
-      }
-
-      api.editMessageText(
-        "Another moment of waiting please",
-        mainApi.chat.id,
-        mainApi.message_id,
-      );
-      const newData = await axios
-        .get(
-          `https://kaiz-ytmp4-downloader.vercel.app/ytmp4?url=${encodeURI(data.url)}&quality=mp3`,
-        )
-        .then((res) => {
-          return res.data;
-        })
-        .catch((error) => {
-          console.error(error);
-          return null;
-        });
-      console.log(`==> ${JSON.stringify(newData, null, 2)} <==`);
-      if (!newData) {
-        return api
-          .editMessageText(
-            `There's an error occured`,
-            mainApi.chat.id,
-            mainApi.message_id,
-          )
-          .then((r) => {
-            setTimeout(() => {
-              api.deleteMessage(r.chat.id, r.message_id);
-            }, 2500);
-          })
-          .catch((e) => {});
-      }
-      const filename = `${__dirname}/../temp/${data.title.replace(/\W/gi, " ").trim().replace(/ /, "_")}.mp3`;
-      const file = fs.createWriteStream(filename);
-
-      http.get(newData.download_url, (res) => {
-        res.pipe(file);
-        file.on("finish", () => {
-          api
-            .sendAudio(msg.chat.id, fs.createReadStream(filename), {}, {})
-            .then((r) => {
-              setTimeout(() => {
-                api.deleteMessage(mainApi.chat.id, mainApi.message_id);
-              }, 2500);
-              // console.log(`INFO: ${JSON.stringify(r)}`);
-              if (fs.existsSync(filename)) {
-                setTimeout(() => {
-                  if (fs.existsSync(filename)) {
-                    fs.unlinkSync(filename, (e) => {});
-                  }
-                }, 10000);
-              }
-            })
-            .catch((error) => {
-              api
-                .editMessageText(
-                  "There's a problem in the file",
-                  mainApi.chat.id,
-                  mainApi.message_id,
-                )
-                .then((r) => {
-                  setTimeout(() => {
-                    api.deleteMessage(mainApi.chat.id, mainApi.message_id);
-                  }, 2500);
-                })
-                .catch((e) => {});
-            });
-        });
-      });
+    .sendMessage(msg.chat.id, `Now looking for ${search}.\nPlease wait ...`)
+    .then((r) => {
+      setTimeout(() => {
+        api.deleteMessage(r.chat.id, r.message_id);
+      }, 2500);
     })
     .catch((e) => {});
+  let q = "?q=";
+  const data = await axios
+    .get(
+      `https://kaiz-apis.gleeze.com/api/ytsearch${q}${encodeURIComponent(search)}`,
+    )
+    .then((r) => {
+      return r.data.items[0];
+    })
+    .catch((err) => {
+      return null;
+    });
+  if (!data) {
+    return api
+      .sendMessage(msg.chat.id, `There's an error occured`)
+      .then((r) => {
+        setTimeout(() => {
+          api.deleteMessage(r.chat.id, r.message_id);
+        }, 2500);
+      })
+      .catch((e) => {});
+  }
+
+  api
+    .sendMessage(msg.chat.id, `Song found[${data.title}]`)
+    .then((r) => {
+      setTimeout(() => {
+        api.deleteMessage(r.chat.id, r.message_id);
+      }, 2500);
+    })
+    .catch((e) => {});
+
+  const newData = await axios
+    .get(
+      `https://kaiz-ytmp4-downloader.vercel.app/ytmp4?url=${encodeURI(data.url)}&quality=mp3`,
+    )
+    .then((res) => {
+      return res.data;
+    })
+    .catch((error) => {
+      console.error(error);
+      return null;
+    });
+  if (!newData) {
+    return api
+      .sendMessage(msg.chat.id, `There's an error occured. Please try again.`)
+      .then((r) => {
+        setTimeout(() => {
+          api.deleteMessage(r.chat.id, r.message_id);
+        }, 2500);
+      })
+      .catch((e) => {});
+  }
+  const filename = `${__dirname}/../temp/${data.title.replace(/\W/gi, " ").trim().replace(/\s/gi, "_")}.mp3`;
+  const file = fs.createWriteStream(filename);
+
+  http.get(newData.download_url, (res) => {
+    res.pipe(file);
+    file.on("finish", () => {
+      api
+        .sendAudio(msg.chat.id, fs.createReadStream(filename), {}, {})
+        .then((r) => {
+          if (fs.existsSync(filename)) {
+            setTimeout(() => {
+              if (fs.existsSync(filename)) {
+                fs.unlinkSync(filename, (e) => {});
+              }
+            }, 10000);
+          }
+        })
+        .catch((error) => {});
+    });
+  });
 };
