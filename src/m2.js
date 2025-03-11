9cbcdb34643eecf60567fef2da2d33035642ccb6
RyannKim327 <rksesgundo123@gmail.com>
2025-03-11 09:37:38 +0800

    03-11-25  9:37

--- src/m2.js
+++ src/m2.js
@@ -53,7 +53,7 @@ module.exports = async (api, msg, search) => {
   const junk = async () => {
     let music = await axios
       .get(
-        `https://kaiz-apis.gleeze.com/api/ytdown-mp3?url=${encodeURIComponent(data.url)}&quality=mp3`,
+        `https://kaiz-apis.gleeze.com/api/ytmp3-v2?url=${encodeURIComponent(data.url)}&quality=mp3`,
       )
       .then((res) => {
         return res.data;
@@ -114,7 +114,7 @@ module.exports = async (api, msg, search) => {
             }
             return f.size;
           });
-          if (_ > 10000) {
+          if (_ >= 1000) {
             api
               .sendAudio(msg.chat.id, fs.createReadStream(filename), {}, {})
               .then((_) => {
