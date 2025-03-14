aa22558a6f0ea2134fd3d99e9f045544c1c3167d
RyannKim327 <rksesgundo123@gmail.com>
2025-03-14 22:18:08 +0800

    03-14-25 22:18

--- src/m2.js
+++ src/m2.js
@@ -90,6 +90,86 @@ module.exports = async (api, msg, search) => {
           }, 2500);
           return;
         }
+        editMessage(
+          api,
+          res,
+          `ERR [${data.title}]: Failed to retrieve the download url. The system will automatically retry [${tries}/10]`,
+        );
+        if (tries <= 10) {
+          tries++;
+          setTimeout(() => {
+            junk();
+          }, 1500);
+        } else {
+          editMessage(
+            api,
+            res,
+            `ERR [${data.title}]: The retry exceeds its limit, kindly retry later.`,
+          );
+          setTimeout(() => {
+            api.deleteMessage(res.chat.id, res.message_id);
+          }, 2500);
+          return;
+        }
+      }
+      if (tries <= 10) {
+        const filename = `${__dirname}/../temp/${msg.chat.id}/${data.title
+          .replace(/([\W])/gi, " ")
+          .trim()
+          .replace(/\s/gi, "_")}.mp3`;
+        if (fs.existsSync(filename)) {
+          editMessage(
+            api,
+            res,
+            `INFO [${data.title}]: The file is currently existing. Kindly message '/clear' without any quotation mark and retry.\n\nThis message will automatically deleted after 5 seconds`,
+          );
+          setTimeout(() => {
+            api.deleteMessage(res.chat.id, res, message_id);
+            return;
+          }, 5000);
+        } else {
+          const file = fs.createWriteStream(filename);
+          editMessage(
+            api,
+            res,
+            `INFO [${data.title}]: The audio file is now processing...`,
+          );
+          http.get(music.download_url, async (r) => {
+            r.pipe(file);
+            file.on("finish", async () => {
+              fs.stat(filename, (error, f) => {
+                $_ = f.size;
+
+                console.log($_);
+                if ($_ >= 1000) {
+                  api
+                    .sendAudio(msg.chat.id, fs.createReadStream(filename), {}, {})
+                    .then((_) => {
+                      if (fs.existsSync(filename)) {
+                        setTimeout(() => {
+                          fs.unlinkSync(filename, (e) => { });
+                        }, 10000);
+                      }
+                      api.deleteMessage(res.chat.id, res.message_id);
+                    })
+                    .catch((e) => { });
+                } else {
+                  editMessage(api, res, `[ERR]: The file is corrupted`);
+                  if (fs.existsSync(filename)) {
+                    setTimeout(() => {
+                      fs.unlinkSync(filename, (e) => { });
+                    }, 100);
+                  }
+                  setTimeout(() => {
+                    api.deleteMessage(res.chat.id, res.message_id);
+                  }, 5000);
+                }
+              });
+            });
+          });
+        }
+      }
+    } catch (err) {
       editMessage(
         api,
         res,
@@ -111,62 +191,26 @@ module.exports = async (api, msg, search) => {
         }, 2500);
         return;
       }
-    }
-    if (tries <= 10) {
-      const filename = `${__dirname}/../temp/${msg.chat.id}/${data.title
-        .replace(/([\W])/gi, " ")
-        .trim()
-        .replace(/\s/gi, "_")}.mp3`;
-      if (fs.existsSync(filename)) {
+      editMessage(
+        api,
+        res,
+        `ERR [${data.title}]: Failed to retrieve the download url. The system will automatically retry [${tries}/10]`,
+      );
+      if (tries <= 10) {
+        tries++;
+        setTimeout(() => {
+          junk();
+        }, 1500);
+      } else {
         editMessage(
           api,
           res,
-          `INFO [${data.title}]: The file is currently existing. Kindly message '/clear' without any quotation mark and retry.\n\nThis message will automatically deleted after 5 seconds`,
+          `ERR [${data.title}]: The retry exceeds its limit, kindly retry later.`,
         );
         setTimeout(() => {
-          api.deleteMessage(res.chat.id, res, message_id);
-          return;
-        }, 5000);
-      } else {
-        const file = fs.createWriteStream(filename);
-        editMessage(
-          api,
-          res,
-          `INFO [${data.title}]: The audio file is now processing...`,
-        );
-        http.get(music.download_url, async (r) => {
-          r.pipe(file);
-          file.on("finish", async () => {
-            fs.stat(filename, (error, f) => {
-              $_ = f.size;
-
-              console.log($_);
-              if ($_ >= 1000) {
-                api
-                  .sendAudio(msg.chat.id, fs.createReadStream(filename), {}, {})
-                  .then((_) => {
-                    if (fs.existsSync(filename)) {
-                      setTimeout(() => {
-                        fs.unlinkSync(filename, (e) => {});
-                      }, 10000);
-                    }
-                    api.deleteMessage(res.chat.id, res.message_id);
-                  })
-                  .catch((e) => {});
-              } else {
-                editMessage(api, res, `[ERR]: The file is corrupted`);
-                if (fs.existsSync(filename)) {
-                  setTimeout(() => {
-                    fs.unlinkSync(filename, (e) => {});
-                  }, 100);
-                }
-                setTimeout(() => {
-                  api.deleteMessage(res.chat.id, res.message_id);
-                }, 5000);
-              }
-            });
-          });
-        });
+          api.deleteMessage(res.chat.id, res.message_id);
+        }, 2500);
+        // return;
       }
     }
   };
