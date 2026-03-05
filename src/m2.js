d14fd3776c11176fa5c8770eb0f00c578ff4336b
RyannKim327 <rksesgundo123@gmail.com>
2026-03-06 05:13:32 +0800

    03-06-26  5:13

--- src/m2.js
+++ src/m2.js
@@ -73,6 +73,7 @@ module.exports = async (api, msg, search) => {
     `INFO [${data.title}]: The audio file is now processing...`,
   );
   log("MUSIC", JSON.stringify(data, null, 2));
+
   http.get(
     data.url,
     {
